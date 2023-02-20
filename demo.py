20e5d54c926888736e557025692266217c750fe8
muriukikendi09 <muriukikendi09@gmail.com>
2023-02-20 13:59:21 +0300

    pythonbasics

---
+++ demo.py
@@ -0,0 +1,20 @@
+my_list = [1 ,2 ,3 ,4]
+my_list[2] = 5
+my_list.append(6)
+my_list = [1, 2, 3, 4]
+my_list[2] = 5  # change the third element to 5
+my_list.append(6)  # add a new element to the end of the list
+print(my_list)
+dict ={"apple": 2, "banana" : 4}
+print(dict["apple"])
+del dict["apple"]
+print(dict)
+head = "head"
+eyes = 2
+body = f"the {head} has {eyes} eyes"
+print(body)
+string = "Doreen"
+first_letter = string[0]
+print (first_letter)
+num = [2+4] * 5
+print(num)
